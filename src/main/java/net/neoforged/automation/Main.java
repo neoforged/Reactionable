0fcf655b9941779454446253e284343629f3104f
Matyrobbrt <matyrobbrt@gmail.com>
2024-08-05 23:52:08 +0300

    Initial work

---
+++ src/main/java/net/neoforged/automation/Main.java
@@ -0,0 +1,11 @@
+package net.neoforged.automation;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
